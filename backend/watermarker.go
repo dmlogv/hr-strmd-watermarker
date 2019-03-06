981ffab98dddcc0534cebd56e8cde7171be731b4
Dmitry Logvinenko <5diezfun@gmail.com>
2019-03-06 22:15:38 +0300

    Modify project structure

--- backend/watermarker.go
+++ backend/watermarker.go
@@ -1,6 +1,23 @@
 package main
 
-const (
-	maxWidth  = 1024
-	maxHeight = 768
+import (
+	"flag"
+	"log"
+	"./imageutils"
 )
+
+
+func main() {
+	server := flag.Bool("server", false, "Server mode")
+	imagePathPtr := flag.String("image", "", "Image file path")
+	watermarkPathPtr := flag.String("watermark", "", "Watermark file path")
+
+	if *server {
+		log.Print("Server mode on")
+	} else {
+		if *imagePathPtr == "" || *watermarkPathPtr == "" {
+			log.Panic("-image or -watermark paths did not present")
+			Panic
+		}
+	}
+}
