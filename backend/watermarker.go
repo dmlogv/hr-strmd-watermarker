0ffa794348e825141a0404219dc22bb4a955da3b
Dmitry Logvinenko <5diezfun@gmail.com>
2019-03-07 17:44:42 +0300

    Implement FileServer with valid MIME-types

--- backend/watermarker.go
+++ backend/watermarker.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"log"
+	"mime"
+	"net/http"
+	"path/filepath"
 )
 
 func main() {
@@ -10,11 +13,42 @@ func main() {
 	imagePathPtr := flag.String("image", "", "Image file path")
 	watermarkPathPtr := flag.String("watermark", "", "Watermark file path")
 
+	flag.Parse()
+
 	if *server {
 		log.Print("Server mode on")
+
+		runServer()
 	} else {
 		if *imagePathPtr == "" || *watermarkPathPtr == "" {
 			log.Fatal("-image or -watermark paths did not present")
 		}
 	}
+}
+
+// MimeFileServer implements FileServer with valid MIME-types
+func MimeFileServer(fs http.Handler) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		uri := r.RequestURI
+		ext := filepath.Ext(uri)
+		mimeType := mime.TypeByExtension(ext)
+
+		if mimeType == "" {
+			mimeType = "text/html"
+		}
+
+		log.Printf("%s %s %s", uri, ext, mimeType)
+
+		w.Header().Set("Content-Type", mimeType)
+		http.StripPrefix("/", fs).ServeHTTP(w, r)
+	}
+}
+
+func runServer() {
+	fs := http.FileServer(http.Dir("./dist"))
+	mimeFs := MimeFileServer(fs)
+
+	http.HandleFunc("/", mimeFs)
+
+	http.ListenAndServe(":3210", nil)
 }
