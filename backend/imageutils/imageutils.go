d97640a7e051a54e3a1921e6223e2685c6506a53
Dmitry Logvinenko <5diezfun@gmail.com>
2019-03-11 18:08:23 +0300

    Implement tiling functions and some helpers

--- backend/imageutils/imageutils.go
+++ backend/imageutils/imageutils.go
@@ -1,7 +1,10 @@
 package imageutils
 
 import (
+	"errors"
 	"image"
+	"image/draw"
+	"math"
 
 	"github.com/nfnt/resize"
 )
@@ -20,4 +23,85 @@ func resizeImage(maxSize uint, decoded image.Image) image.Image {
 	}
 
 	return resize.Resize(targetWidth, targetHeight, decoded, resize.Lanczos3)
+}
+
+// OverlayImage tiles a centered overlay image over the base
+func OverlayImage(base image.Image, overlay image.Image) image.Image {
+	baseBounds := base.Bounds()
+
+	overlayed := image.NewRGBA(baseBounds)
+	draw.Draw(overlayed, baseBounds, base, image.ZP, draw.Src)
+
+	return overlayed
+}
+
+// ceilToOdd rounds x up to nearest odd number
+func ceilToOdd(x float64) int {
+	ceil := int(math.Ceil(x))
+
+	if isOdd(ceil) {
+		return ceil
+	}
+
+	return ceil + 1
+}
+
+// isOdd test x is odd number
+func isOdd(x int) bool {
+	return x%2 != 0
+}
+
+// tileNum returns a number of tile images in the needed size
+func tileNum(boundSize int, tileSize int) int {
+	return ceilToOdd(float64(boundSize) / float64(tileSize))
+}
+
+// getInitCoord returns start co-ordinate for image tiling in bounds
+func getInitCoord(boundWidth int, tileWidth int, n int) int {
+	return (boundWidth - n*tileWidth) / 2
+}
+
+// align is an image aligns enum
+type align int
+
+const (
+	tl align = iota
+	tc
+	tr
+	cl
+	cc
+	cr
+	bl
+	bc
+	br
+)
+
+// newTiledImage returns a new image tiled with img in b bounds
+func newTiledImage(img image.Image, b image.Rectangle, a align) (image.Image, error) {
+	tiled := image.NewRGBA(b)
+	w := img.Bounds().Dx()
+	h := img.Bounds().Dy()
+
+	if a != cc {
+		return nil, errors.New("Alignments except align.cc are not implemented")
+	}
+
+	tiled = image.NewRGBA(b)
+
+	// Number of images on the axes
+	xNum := tileNum(b.Dx(), w)
+	yNum := tileNum(b.Dy(), h)
+
+	// Initial co-ordinates
+	x := getInitCoord(b.Dx(), w, xNum)
+	y := getInitCoord(b.Dy(), h, yNum)
+
+	for kx := 0; kx < xNum; kx++ {
+		for ky := 0; ky < yNum; ky++ {
+			p := image.Point{x + kx*w, y + ky*h}
+			draw.Draw(tiled, img.Bounds(), img, p, draw.Over)
+		}
+	}
+
+	return tiled, nil
 }
