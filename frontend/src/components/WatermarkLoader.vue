2baaa0120a727c4985ce98ca9184a5baf9502f22
Dmitry Logvinenko <5diezfun@gmail.com>
2019-03-12 18:03:02 +0300

    Implement files uploading/downloading

--- frontend/src/components/WatermarkLoader.vue
+++ frontend/src/components/WatermarkLoader.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="wm-loader">
     <h1>Watermark creator</h1>
-    <form @submit="checkForm" action="/" method="post">
+    <div class="form" v-on:change="handleFileChange()" >
       <div class="item">
         <label for="imageFile">Image File</label>
         <input id="imageFile" type="file" accept="image/jpeg" />
@@ -10,14 +10,56 @@
         <label for="watermarkFile">Watermark File</label>
         <input id="watermarkFile" type="file" accept="image/x-png" />
       </div>
-    </form>
+
+      <div class="control">
+        <button v-on:click="upload()">Send</button>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
+  let axios = require("axios");
+  let fileDownload = require('js-file-download');
+
   export default {
-    name: 'WatermarkLoader'
+    name: 'WatermarkLoader',
+    data() {
+      return {
+        images: {}
+      }
+    },
+    methods: {
+      handleFileChange() {
+        console.log(this);
+        this.$el.querySelectorAll("input[type='file']").forEach(f => {
+          this.images[f.id] = f.files[0];
+        });
+      },
+      upload() {
+        let formData = new FormData();
+
+        Object.keys(this.images).forEach(k => {
+          formData.append(k, this.images[k]);
+        });
+
+        axios.post(
+          "",
+          formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            },
+          responseType: 'blob'
+        }).then(response => {
+          console.log('Sended');
+          fileDownload(response.data, 'result.jpg');
+         }).catch(response => {
+          console.log('Failed');
+        });
+
+      }
+    }
   }
 </script>
 
@@ -29,7 +71,7 @@
     justify-content: center;
   }
 
-  form {
+  .form {
     display: table;
     border-spacing: 1em .5em
   }
